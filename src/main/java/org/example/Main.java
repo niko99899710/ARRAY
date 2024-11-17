0a0217b71ff1cb07b93de42221fb4fc81f663af3
karinaid <bagdasarova-k@inbox.ru>
2024-11-18 01:03:10 +0400

    H/W 1/1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+        task2 ();
+        task3 ();
+        System.out.println();
+    }
+    public static void task1() {
+        System.out.println("Задание 1");
+
+        int[] array = {3, 87, 12, 98, 2, 4, 66};
+        for (int i = array.length - 1; i >=0 ; i --) {
+            System.out.println(array[i]);
+        }
+
+    }
+    public static void task2 () {
+        System.out.println("Задание 2");
+
+    }
+    public static void task3 () {
+        System.out.println("Задание 3");
+        int[] array = {6, 117, 44, 5, 32, 81, 9};
+        for (int i = 0; i < array.length; i ++) {
+         //   if (i % 2 == 0) {
+          //      array [i] = i;
+            array [i] = (i +1) % 2;
+            System.out.println(array[i]);
+        }
     }
 }
