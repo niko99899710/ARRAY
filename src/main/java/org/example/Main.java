b4f353927cae8991c884acc08d9b687a370e301e
karinaid <bagdasarova-k@inbox.ru>
2024-11-18 09:41:02 +0400

    H/W 2/2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,49 @@
 package org.example;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        task1 ();
-        task2 ();
-        task3 ();
-        System.out.println();
+        task1();
+        task2();
+        double[] array = {0.81, 0.22, 0.47};
+        System.out.println(Arrays.toString(array1(array)));
+        task3();
     }
+
     public static void task1() {
         System.out.println("Задание 1");
-
         int[] array = {3, 87, 12, 98, 2, 4, 66};
-        for (int i = array.length - 1; i >=0 ; i --) {
+        for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i]);
         }
-
     }
-    public static void task2 () {
+
+    private static void task2() {
         System.out.println("Задание 2");
-
     }
-    public static void task3 () {
+
+    public static double[] array1(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+
+    public static void task3() {
         System.out.println("Задание 3");
         int[] array = {6, 117, 44, 5, 32, 81, 9};
-        for (int i = 0; i < array.length; i ++) {
-         //   if (i % 2 == 0) {
-          //      array [i] = i;
-            array [i] = (i +1) % 2;
-            System.out.println(array[i]);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
         }
     }
 }
